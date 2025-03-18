d02b2e5a9a3bb5f99de2719c5bddbc559d14cada
Raphael <rookie17@naver.com>
2025-03-18 11:50:11 +0900

    Completed Input System

--- D1/Source/D1/Player/D1PlayerController.cpp
+++ D1/Source/D1/Player/D1PlayerController.cpp
@@ -2,4 +2,64 @@
 
 
 #include "Player/D1PlayerController.h"
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
 
+AD1PlayerController::AD1PlayerController(const FObjectInitializer& ObjectInitializer)
+	: Super(ObjectInitializer)
+{
+}
+
+void AD1PlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+
+	auto* Subsystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer());
+	if (Subsystem)
+	{
+		Subsystem->AddMappingContext(InputMappingContext , 0);
+	}
+}
+
+void AD1PlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+
+	auto* EnhancedInputComponent = Cast<UEnhancedInputComponent>(InputComponent);
+	if (EnhancedInputComponent)
+	{
+		EnhancedInputComponent->BindAction(AttackAction , ETriggerEvent::Triggered , this , &AD1PlayerController::Input_Attack);
+		EnhancedInputComponent->BindAction(MoveAction , ETriggerEvent::Triggered , this , &AD1PlayerController::Input_Move);
+		EnhancedInputComponent->BindAction(TurnAction , ETriggerEvent::Triggered , this , &AD1PlayerController::Input_Turn);
+
+	}
+}
+
+void AD1PlayerController::Input_Attack(const FInputActionValue& InputValue)
+{
+	GEngine->AddOnScreenDebugMessage(0 , 1.0f , FColor::Cyan , TEXT("Attack"));
+}
+
+void AD1PlayerController::Input_Move(const FInputActionValue& InputValue)
+{
+	FVector2D MovementVector = InputValue.Get<FVector2D>();
+
+	if (MovementVector.X != 0)
+	{
+		FVector Direction = GetPawn()->GetActorForwardVector() * MovementVector.X;
+		GetPawn()->AddActorWorldOffset(Direction);
+	}
+
+	if (MovementVector.Y != 0)
+	{
+		FVector Direction = GetPawn()->GetActorRightVector() * MovementVector.Y;
+		GetPawn()->AddActorWorldOffset(Direction);
+	}
+}
+
+void AD1PlayerController::Input_Turn(const FInputActionValue& InputValue)
+{
+	float XValue = InputValue.Get<float>();
+	FRotator Rotation(0.0f , XValue , 0.0f);
+	GetPawn()->AddActorWorldRotation(Rotation);
+}
--- D1/Source/D1/Player/D1PlayerController.h
+++ D1/Source/D1/Player/D1PlayerController.h
@@ -6,6 +6,8 @@
 #include "GameFramework/PlayerController.h"
 #include "D1PlayerController.generated.h"
 
+struct FInputActionValue;
+
 /**
  * 
  */
@@ -14,4 +16,28 @@ class D1_API AD1PlayerController : public APlayerController
 {
 	GENERATED_BODY()
 	
+public:
+	AD1PlayerController(const FObjectInitializer& ObjectInitializer = FObjectInitializer::Get());
+
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+
+private:
+	void Input_Attack(const FInputActionValue& InputValue);
+	void Input_Move(const FInputActionValue& InputValue);
+	void Input_Turn(const FInputActionValue& InputValue);
+
+protected:
+	UPROPERTY(EditAnywhere, Category = Input)
+	TObjectPtr<class UInputMappingContext> InputMappingContext;
+
+	UPROPERTY(EditAnywhere , Category = Input)
+	TObjectPtr<class UInputAction> AttackAction;
+
+	UPROPERTY(EditAnywhere , Category = Input)
+	TObjectPtr<class UInputAction> MoveAction;
+
+	UPROPERTY(EditAnywhere , Category = Input)
+	TObjectPtr<class UInputAction> TurnAction;
 };
