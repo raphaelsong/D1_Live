50415dc0e6ab6fad5160bb5de817ba176b2fe98b
Raphael <rookie17@naver.com>
2025-03-20 14:40:04 +0900

    Completed Character Class

--- D1/Source/D1/Character/D1Character.cpp
+++ D1/Source/D1/Character/D1Character.cpp
@@ -4,6 +4,7 @@
 #include "Character/D1Character.h"
 #include "GameFramework/SpringArmComponent.h"
 #include "Camera/CameraComponent.h"
+#include "GameFramework/CharacterMovementComponent.h"
 
 // Sets default values
 AD1Character::AD1Character()
@@ -11,13 +12,27 @@ AD1Character::AD1Character()
  	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
 	PrimaryActorTick.bCanEverTick = true;
 
+	// Pawn
+	bUseControllerRotationPitch = false;
+	bUseControllerRotationYaw = false;
+	bUseControllerRotationRoll = false;
+
 	SpringArm = CreateDefaultSubobject<USpringArmComponent>(TEXT("SpringArm"));
 	SpringArm->SetupAttachment(RootComponent);
 	SpringArm->TargetArmLength = 700.0f;
 	SpringArm->SetRelativeRotation(FRotator(-30.0f , 0.0f , 0.0f));
+	SpringArm->bUsePawnControlRotation = true;
+	SpringArm->bInheritPitch = false;
+	SpringArm->bInheritYaw = true;
+	SpringArm->bInheritRoll = false;
 
 	Camera = CreateDefaultSubobject<UCameraComponent>(TEXT("Camera"));
 	Camera->SetupAttachment(SpringArm);
+	Camera->bUsePawnControlRotation = false;
+
+	// Movement
+	GetCharacterMovement()->bOrientRotationToMovement = true;
+	GetCharacterMovement()->RotationRate = FRotator(0.0f , 720.0f , 0.0f);
 }
 
 // Called when the game starts or when spawned
